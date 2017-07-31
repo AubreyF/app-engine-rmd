ae63e83fed96f529f1c214535264b5f9755733a2
Aubrey Falconer <afalconer@kochava.com>
2017-07-30 19:12:19 -1000

    * Added Notes to Readme. * Upgraded project to from Django 1.2 to Django 1.11. > Upgraded settings to play nicely with latest Django template config.

--- README.md
+++ README.md
@@ -4,14 +4,19 @@
 
 # App Engine Snippets
 ```
+# Local Testing Setup
+pip install -r requirements.txt -t lib/
+
 # Local Testing
 dev_appserver.py app.yaml
 
-# Production Deployment
+# Production Deployment Setup
 gcloud auth login
 gcloud config set project rmd2-1037
+
+# Production Deployment
 gcloud app deploy app.yaml index.yaml
 
-# Visit Porduction Site
+# Visit Production Site
 gcloud app browse
 ```
--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python
 import os
 import webapp2
-from django.template.loaders.filesystem import Loader
 from django.template.loader import render_to_string
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 
-SLIDE_LIST = {'tipplewood': 3, 'willowbend': 13, 'woodrun4': 9, 'woodrun5': 6, 'sanjuan': 9, 'westlake': 5, 'beverlyhills': 0, 'color_sketches': 10, 'tortuga': 9, 'thousandoaks': 6,
+SLIDE_LIST = {'tipplewood': 3, 'willowbend': 13, 'woodrun4': 9, 'woodrun5': 6, 'sanjuan': 9, 'westlake': 5, 'beverlyhills': 0, 'color_sketches': 10, 'tortuga': 9,
+              'thousandoaks': 6,
               'hombeyhills': 11, 'ecocity': 5, 'seattle': 0, 'sterlingchandler': 5, 'woodrun7': 5, 'snowmassclub': 8, 'casadeemdeko': 3, 'puertovallarta': 0, 'illinois': 5,
               'sherekhan': 9, 'source': 0, 'kahala': 4, 'maunakea': 5, 'waterford': 13, 'indiana': 7, 'black_and_white_sketches': 10, 'wilshire': 7, 'kiddisland': 2, 'hahn': 5,
               'palmsprings': 2, 'orcas': 5, 'snowmassranch': 4, 'snowmass7': 7, 'thepark': 7, 'arrowpoint': 2, 'medina': 8, 'robertrose': 8, 'woodrun': 0, 'ladera': 7,
               'makanikaimarina': 4, 'napavalley': 5, 'lot8': 5, 'sandpoint': 4, 'stoneybrook': 2, 'waiikii': 3, 'lot2': 5, 'lot1': 8, 'hawaii': 6, 'exotica': 5, 'serenity': 12}
+
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
@@ -45,8 +46,8 @@ class SlideShow(webapp2.RequestHandler):
                 data += "'%s.jpg'" % (img,)
                 # thumbs += '<li><a href="#slide-%d"><img src="/img/gallery/%s.jpg" width="50" height="40" /></a></li>' % (i, img)
 
-        # Generate custom image list
         else:
+            # Generate custom image list
             for img in args.split('-'):
                 i += 1
                 img = title + "/" + img;
@@ -63,7 +64,6 @@ class SlideShow(webapp2.RequestHandler):
         }
         self.response.out.write(render_to_string('slideshow.html', template_values))
         self.response.headers.add_header('Cache-Control', 'max-age=3600')
-
 
 app = webapp2.WSGIApplication([
     ('/show/(.*)/(.*)', SlideShow),
